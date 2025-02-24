0e7f199e4fb2c7df139900ce12d3fa586c41e874
Gleb Rudko <glebrudko@inbox.ru>
2025-02-24 17:37:19 +0300

    add intents

--- app/src/main/java/com/denica/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/denica/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.denica.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaButton =  findViewById<Button>(R.id.media_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        searchButton.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
+        mediaButton.setOnClickListener {
+            val intent = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(intent)
+        }
+        settingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
         }
     }
 }
