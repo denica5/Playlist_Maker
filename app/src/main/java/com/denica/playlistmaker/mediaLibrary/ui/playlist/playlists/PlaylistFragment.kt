038fa901d12f3d3d139265e03904603b5c351b4d
Gleb Rudko <glebrudko@inbox.ru>
2025-12-17 09:49:18 +0300

    Домашнее задание 27 | Уровень 1

--- app/src/main/java/com/denica/playlistmaker/mediaLibrary/ui/playlist/playlists/PlaylistFragment.kt
+++ app/src/main/java/com/denica/playlistmaker/mediaLibrary/ui/playlist/playlists/PlaylistFragment.kt
@@ -1,98 +1,146 @@
 package com.denica.playlistmaker.mediaLibrary.ui.playlist.playlists
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.core.view.isVisible
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.GridLayoutManager
-import com.denica.playlistmaker.databinding.FragmentPlaylistsBinding
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import com.denica.playlistmaker.R
 import com.denica.playlistmaker.mediaLibrary.domain.Playlist
-import com.denica.playlistmaker.mediaLibrary.ui.MediaLibraryFragmentDirections
-import com.denica.playlistmaker.search.ui.SearchFragment
-import com.denica.playlistmaker.utils.BindingFragment
-import com.denica.playlistmaker.utils.debounce
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.denica.playlistmaker.mediaLibrary.ui.playlist.playlists.PlaylistViewModel.Companion.pluralizeTracks
+import com.denica.playlistmaker.search.ui.Loading
+import com.denica.playlistmaker.search.ui.NothingFound
 
 
-class PlaylistFragment : BindingFragment<FragmentPlaylistsBinding>() {
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun PlaylistsScreen(
+    playlistState: PlaylistState,
+    onPlaylistClick: (Playlist) -> Unit,
+    onAddPlaylistClick: () -> Unit
+) {
 
-    val viewModel by viewModel<PlaylistViewModel>()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-    }
-
-    override fun createBinding(
-        inflater: LayoutInflater,
-        container: ViewGroup?
-    ): FragmentPlaylistsBinding {
-        return FragmentPlaylistsBinding.inflate(inflater, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.mediaLibraryButtonAddNewPlaylist.setOnClickListener {
-            findNavController().navigate(
-                MediaLibraryFragmentDirections.actionMediaLibraryFragmentToCreatePlaylistFragment()
+    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(
+            onClick = onAddPlaylistClick,
+            modifier = Modifier.padding(top = 24.dp, bottom = 16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.onPrimary
+            )
+        ) {
+            Text(
+                stringResource(R.string.media_library_button_add_new_playlist),
+                style = MaterialTheme.typography.bodyMedium,
+                fontSize = 14.sp,
+                color = MaterialTheme.colorScheme.primary
             )
         }
 
-        val onPlaylistDebounce = debounce<Playlist>(
-            SearchFragment.CLICK_DEBOUNCE_DELAY,
-            viewLifecycleOwner.lifecycleScope,
-            false
-        ) { playlist ->
-            findNavController().navigate(
-                MediaLibraryFragmentDirections.actionMediaLibraryFragmentToPlaylistDetailFragment(playlist)
-            )
-        }
-        val adapter = PlaylistAdapter(onPlaylistDebounce)
-        binding.playlistRecycler.adapter = adapter
-        binding.playlistRecycler.layoutManager = GridLayoutManager(requireContext(), 2)
-        viewModel.observePlaylistState().observe(viewLifecycleOwner) {
-            when (it) {
-                is PlaylistState.Loading -> {
-                    binding.playlistProgressbar.isVisible = true
-                    binding.playlistPlaceholderNoPlaylist.isVisible = false
-                    binding.playlistRecycler.isVisible = false
-                }
+        when (playlistState) {
+            is PlaylistState.Loading -> {
+                Loading()
+            }
 
-                is PlaylistState.Empty -> {
-                    binding.playlistProgressbar.isVisible = false
-                    binding.playlistPlaceholderNoPlaylist.isVisible = true
-                    binding.playlistRecycler.isVisible = false
-                }
+            is PlaylistState.Empty -> {
+                NothingFound(R.string.media_library_playlists_empty)
+            }
 
-                is PlaylistState.Content -> {
-                    binding.playlistProgressbar.isVisible = false
-                    binding.playlistPlaceholderNoPlaylist.isVisible = false
-                    binding.playlistRecycler.isVisible = true
-                    adapter.itemList = it.data
-                    adapter.notifyDataSetChanged()
+            is PlaylistState.Content -> {
+
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    contentPadding = PaddingValues(horizontal = 16.dp),
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    items(playlistState.data) { playlist ->
+                        PlaylistItem(playlist) { onPlaylistClick(playlist) }
+                    }
                 }
             }
         }
     }
+}
 
-    override fun onResume() {
-        super.onResume()
-        viewModel.getPlaylists()
-    }
+@Composable
+fun PlaylistItem(
+    playlist: Playlist,
+    modifier: Modifier = Modifier,
+    onClick: ((Playlist) -> Unit)
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 4.dp)
+            .clickable { onClick(playlist) }
 
-    override fun onStop() {
-        super.onStop()
+    ) {
 
-    }
 
-    companion object {
+        AsyncImage(
+            model = playlist.imagePath,
+            contentDescription = null,
+            placeholder = painterResource(R.drawable.ic_track_placeholder),
+            error = painterResource(R.drawable.ic_track_placeholder),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(160.dp)
+                .clip(RoundedCornerShape(8.dp))
+        )
 
-        @JvmStatic
-        fun newInstance() =
-            PlaylistFragment().apply {
 
-            }
+        Text(
+            text = playlist.name,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontFamily = FontFamily(Font(R.font.ys_display_regular)),
+            fontSize = 12.sp,
+            lineHeight = 16.sp,
+            color = MaterialTheme.colorScheme.onSecondary,
+            modifier = Modifier.padding(top = 4.dp)
+        )
+
+
+        Text(
+            text =
+                String.format(
+                    stringResource(
+                        pluralizeTracks(
+                            playlist.trackCount
+                        )
+                    ), playlist.trackCount
+                ),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontFamily = FontFamily(Font(R.font.ys_display_regular)),
+            fontSize = 12.sp,
+            lineHeight = 16.sp,
+            color = MaterialTheme.colorScheme.onSecondary
+        )
     }
 }
